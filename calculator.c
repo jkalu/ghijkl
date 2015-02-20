bafded0eb5c00dff8ff9ac3330f92c957ab2cf61
jkalu <jhoomkalu@gmail.com>
2015-02-20 16:53:09 +0530

    Added a function to add tw numbers.

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+long long int add(long long int a,long long int b)
+{
+return a+b;
+}
+
+int main()
+{
+long long int a,b;
+scanf("%lld %lld",&a,&b);
+printff("%lld\n",add(a,b));
+return 0;
+}
