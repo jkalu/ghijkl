8b9e50e282a38cb0c4c92aa28aeda15be7d002da
jkalu <jhoomkalu@gmail.com>
2015-02-20 17:16:31 +0530

    few changes

--- calculator.c
+++ calculator.c
@@ -4,23 +4,17 @@ long long int add(long long int a,long long int b)
 {
 return a+b;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 double div(long long int a, long long int b)
 {
 if(b==0)
 return 404;
 return ((double)((double)a)/((double)b));
-=======
 long long int diff(long long int a, long long int b)
 {
 return a-b;
->>>>>>> Function to find the difference added
-=======
 long long int diff(long long int a, long long int b)
 {
 return a-b;
->>>>>>> ed3583a3f3e4b36612446edcdf30b0208ddfd8aa
 }
 
 int main()
